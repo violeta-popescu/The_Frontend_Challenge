cbc2ed4f0c83f7052eb9a5f63982727207ed0079
violeta-popescu <violeta-popescu@live.be>
2015-07-01 19:49:33 +0200

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+(function(){
+    'use strict';
+    
+var app = angular.module('challenge', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+    
+})    
+    
+app.controller('AppController',['$scope','$http',function($scope,$http) { 
+
+    $scope.filter="";
+    
+    $http.get("../tiles.json").success(function(data){
+        
+        $scope.tiles=data;
+
+    });
+    
+    /*$scope.items=[];
+  for(var i=0; i<10;i++){
+    $scope.items[i] = {
+      'color': ('#'+Math.floor(Math.random()*16777215).toString(16))
+    };
+  }*/
+    
+       
+}])
+
+})();    
+                                 
